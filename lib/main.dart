954869184abc6a6761fe7ffbcb9e90336c6a28cb
Hajime <h.morikosi@gmail.com>
2025-05-11 13:17:38 +0900

    Rustと連携します

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,56 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_quill/flutter_quill.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:my_manga_editor/pages/main/main_page.dart';
+// The original content is temporarily commented out to allow generating a self-contained demo - feel free to uncomment later.
 
-void main() {
-  runApp(const ScreenUtilInit(child: ProviderScope(child: MyApp())));
+// import 'package:flutter/material.dart';
+// import 'package:flutter_quill/flutter_quill.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:my_manga_editor/pages/main/main_page.dart';
+//
+// void main() {
+//   runApp(const ScreenUtilInit(child: ProviderScope(child: MyApp())));
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: '漫画プロットエディタ Komatto',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       localizationsDelegates: FlutterQuillLocalizations.localizationsDelegates,
+//       home: const MainPage(),
+//     );
+//   }
+// }
+//
+
+import 'package:flutter/material.dart';
+import 'package:my_manga_editor/src/rust/api/simple.dart';
+import 'package:my_manga_editor/src/rust/frb_generated.dart';
+
+Future<void> main() async {
+  await RustLib.init();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '漫画プロットエディタ Komatto',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
+        body: Center(
+          child: Text(
+              'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`'),
+        ),
       ),
-      localizationsDelegates: FlutterQuillLocalizations.localizationsDelegates,
-      home: const MainPage(),
     );
   }
 }
